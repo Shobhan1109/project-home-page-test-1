233b5f807f4b6c1881b798e87c3598c7415224a5
Shobhan SR <101562221+Shobhan1109@users.noreply.github.com>
2022-04-19 19:07:03 +0530

    Project home page trail

---
+++ main.py
@@ -0,0 +1,10 @@
+# from Flask import flask, render_templates
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route("/")
+def welcome():
+    return render_template("homepage.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
